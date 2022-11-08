946f6b5b40e0cfeb158d1a6fa096c865f36906a5
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2022-11-08 16:22:39 +0100

    feat(gcloud): improve sourcing of gcloud autocompletion

--- roles/mac-dev-playbook/files/.zshrc.d/gcloud.sh
+++ roles/mac-dev-playbook/files/.zshrc.d/gcloud.sh
@@ -4,9 +4,8 @@ alias gclo="gcloud"
 alias gcloalu="gcloud auth login --update-adc"
 
 # Autocompletion
-# FIXME
 # The next line updates PATH for the Google Cloud SDK.
-source /Users/ANTOINE/.asdf/installs/gcloud/390.0.0/path.zsh.inc
+source "$(asdf where gcloud)/path.zsh.inc"
 
 # The next line enables zsh completion for gcloud.
-source /Users/ANTOINE/.asdf/installs/gcloud/390.0.0/completion.zsh.inc
+source "$(asdf where gcloud)/completion.zsh.inc"
