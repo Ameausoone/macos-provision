555a08ad8a50806c73a81a67fa6250e63fd1f869
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2022-11-18 16:09:15 +0100

    feat(python): add script

--- roles/mac-dev-playbook/files/.zshrc.d/gh.sh
+++ roles/mac-dev-playbook/files/.zshrc.d/gh.sh
@@ -5,48 +5,45 @@
 
 
 function gh_copy_issue(){
-  if [ -z "$1" ]; then
-    echo "What issue do you want to copy?"
-    read issue
+  if [ -z "${1}" ]; then
+    issue=$(gh issue list --state closed --limit 100 | fzf --header='What issue do you want to copy ?' --preview 'gh issue view {1}' | awk '{print $1}')
   else
-    issue=$1
+    issue="${1}"
   fi
   local issue_number="${issue}"
   local issue_title=$(gh issue view ${issue_number} --json 'title' --jq '.title')
   echo -e "- Copy of #${issue_number} \n\n $(gh issue view ${issue_number} --json 'body' --jq '.body')" | gh issue create --title "${issue_title}" --web --body-file=-
+  echo "Then run git_cherry_pick_commit "
 }
 
-function git_cherry_pick_issue(){
-  if [ "$1" = "-h" ]; then
-    echo "Usage: git_cherry_pick_issue <issue_number>"
+function git_cherry_pick_commit(){
+  # First checkout on branch where to cherry-pick commit
+  # choose branch main maintenance/v0.7.x of where to cherry-pick
+  echo "First checkout on branch where to cherry-pick commit, ok ? (y/n)"
+  read answer
+  if [ "${answer}" != "y" ]; then
     return
   fi
-  if [ -z "$1" ]; then
-    echo "What issue do you want to copy ?"
-    # list recently closed issues with gh and fzf
-    issue=$(gh issue list --state closed --limit 100 | fzf --preview 'gh issue view {1}' | awk '{print $1}')
-  else
-    issue=$1
-  fi
-  # search linked PR
-    local pr=$(gh pr list --search "is:merged is:closed linked:issue/${issue}" --limit 1 | awk '{print $1}')
-
-
-  if [ -z "$2" ]; then
-    echo "What branch do you want to cherry-pick to? "
-    branch=$(git branch -a | fzf)
-  else
-    branch=$2
-  fi
-  git pull origin ${branch}
-
-  local issue_number="${issue}"
+  echo "What is the id of github issue for the cherry-pick ?"
+  read issue_number
   local issue_title=$(gh issue view ${issue_number} --json 'title' --jq '.title')
   local issue_branch=$(echo "${issue_title}" | tr '[:upper:]' '[:lower:]' | tr ' ' '-' | tr -d '[:punct:]')
-  local issue_branch="issue-${issue_number}-${issue_branch}"
+  local issue_branch="cherry-pick/${issue_number}/${issue_branch}"
+  echo "Checkout on branch [${issue_branch}]"
   git checkout -b "${issue_branch}"
-  gh pr checkout ${issue_number}
-  git cherry-pick -x $(git rev-parse HEAD~1)..HEAD
+
+  echo "Select the branch: [main,maintenance/v0.x.x] where to cherry-pick commit ?"
+  read branch
+  echo "Fetch git branch : [${branch}]."
+  git fetch origin "${branch}" "${branch}"
+
+  pr_number=$(gh pr list --state closed --limit 100 | fzf --header='What PR do you want to copy ?' --preview 'gh pr view {1}' | awk '{print $1}')
+  echo "Cherry-pick commit from PR : [${pr_number}]"
+  commit_to_cherry_pick=$(gh pr view ${pr_number} --json 'mergeCommit' --jq '.mergeCommit.oid')
+  echo "Cherry-pick commit : [${commit_to_cherry_pick}]"
+  git cherry-pick "${commit_to_cherry_pick}"
+
   git push --set-upstream origin "${issue_branch}"
-  gh pr create --fill --web
+  local pr_title="[Cherry-pick] $(gh pr view ${pr_number} --json 'title' --jq '.title')"
+  echo -e "- Copy of #${pr_number} \n - Fix #${issue_number} \n\n $(gh pr view ${pr_number} --json 'body' --jq '.body')" | gh pr create --title "${pr_title}" --web --body-file=-
 }
---
+++ roles/mac-dev-playbook/files/.zshrc.d/python.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+
+alias python_setup_venv="python3 -m venv .venv --upgrade-deps && source .venv/bin/activate && pip install -r requirements.txt"
