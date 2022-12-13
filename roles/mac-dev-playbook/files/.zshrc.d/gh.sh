981742f65c6fcf4a46bc358203a803d69492748c
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2022-12-13 17:03:29 +0100

    feat(gh): new script to duplicate issue and pr

--- roles/mac-dev-playbook/files/.zshrc.d/gh.sh
+++ roles/mac-dev-playbook/files/.zshrc.d/gh.sh
@@ -5,13 +5,22 @@
 
 
 function gh_copy_issue(){
-  if [ -z "${1}" ]; then
-    issue=$(gh issue list --state closed --limit 100 | fzf --header='What issue do you want to copy ?' --preview 'gh issue view {1}' | awk '{print $1}')
+  if [[ -z "${1}" ]]; then
+    #issue=$(gh issue list --state closed --limit 100 | fzf --header='What is the issue that you want to copy ?' --preview 'gh issue view {1}' | awk '{print $1}')
+    echo "What is the issue number that you want to copy ?"
+    read issue
   else
     issue="${1}"
   fi
-  local issue_number="${issue}"
-  local issue_title=$(gh issue view ${issue_number} --json 'title' --jq '.title')
+  if [[ -z "${2}" ]]; then
+    echo "What is the target branch of the new issue ?"
+    read target_branch
+  else
+    target_branch="${2}"
+  fi
+  issue_number="${issue}"
+  issue_title=$(gh issue view ${issue_number} --json 'title' --jq '.title')
+  issue_title="${issue_title} (target ${target_branch})"
   echo -e "- Copy of #${issue_number} \n\n $(gh issue view ${issue_number} --json 'body' --jq '.body')" | gh issue create --title "${issue_title}" --web --body-file=-
   echo "Then run git_cherry_pick_commit "
 }
@@ -24,6 +33,7 @@ function git_cherry_pick_commit(){
   if [ "${answer}" != "y" ]; then
     return
   fi
+  current_branch=$(git rev-parse --abbrev-ref HEAD)
   echo "What is the id of github issue for the cherry-pick ?"
   read issue_number
   issue_title=$(gh issue view ${issue_number} --json 'title' --jq '.title')
@@ -37,7 +47,7 @@ function git_cherry_pick_commit(){
   echo "Checkout on branch [${issue_branch}]"
   git checkout -B "${issue_branch}"
 
-  echo "Select the branch: [main,maintenance/v0.x.x] where to cherry-pick commit ?"
+  echo "Select the branch: [main, maintenance/v0.7.x, maintenance/v0.8.x, ...] where to cherry-pick commit ?"
   read branch
   echo "Fetch git branch : [${branch}]."
   git fetch origin "${branch}" "${branch}"
@@ -48,6 +58,6 @@ function git_cherry_pick_commit(){
   git cherry-pick "${commit_to_cherry_pick}"
 
   git push --set-upstream origin "${issue_branch}"
-  local pr_title="[Cherry-pick] $(gh pr view ${pr_number} --json 'title' --jq '.title')"
+  pr_title="[Cherry-pick] $(gh pr view ${pr_number} --json 'title' --jq '.title') (target ${current_branch})"
   echo -e "- Copy of #${pr_number} \n - Fix #${issue_number} \n\n $(gh pr view ${pr_number} --json 'body' --jq '.body')" | gh pr create --title "${pr_title}" --web --body-file=-
 }
