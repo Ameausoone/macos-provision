9c9a63316c55f5e2c79d97ee1d8598f5b456960b
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2023-04-15 16:46:55 +0200

    feat(ghpc): install ghpc

--- roles/mac-dev-playbook/files/.zshrc.d/core/50_gcloud.zsh
+++ roles/mac-dev-playbook/files/.zshrc.d/core/50_gcloud.zsh
@@ -1,7 +1,12 @@
 #!/usr/bin/env zsh
 
-alias gclo="gcloud"
-alias gcloalu="gcloud auth login --update-adc"
+# Aliases for gcloud
+# Authenticate
+alias g='gcloud'
+alias gauth='gcloud auth login --update-adc'
+alias gke='gcloud container'
+alias gke-list='gcloud container clusters list'
+alias gke-credentials='gcloud container clusters get-credentials'
 
 # Autocompletion
 # The next line updates PATH for the Google Cloud SDK.
@@ -9,3 +14,9 @@ source "$(asdf where gcloud)/path.zsh.inc"
 
 # The next line enables zsh completion for gcloud.
 source "$(asdf where gcloud)/completion.zsh.inc"
+
+# Add hpc toolkit to path
+export PATH="${HOME}/hpc-toolkit:$PATH"
+
+# enable hpc autocompletion
+eval "$(ghpc completion zsh)"
