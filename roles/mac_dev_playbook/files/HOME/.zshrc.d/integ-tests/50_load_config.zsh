7495e691bc3c9397f99311e44af5f1c9a8ff15cf
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2023-05-01 15:14:39 +0200

    feat(integ): setup creds for GCP integ-tests

--- roles/mac_dev_playbook/files/HOME/.zshrc.d/integ-tests/50_load_config.zsh
+++ roles/mac_dev_playbook/files/HOME/.zshrc.d/integ-tests/50_load_config.zsh
@@ -2,3 +2,14 @@
 
 export GCP_PROJECT_ID=$(creds_get_local_secret "com.ameausoone.integ.tests" "gcp_project_id")
 export GCP_REGION=$(creds_get_local_secret "com.ameausoone.integ.tests" "gcp_region")
+export SERVICE_ACCOUNT_JSON=$(creds_get_local_secret "com.ameausoone.integ.tests" "gcp_service_account")
+
+function googlecloud_enable_gcp_apis(){
+  gcloud services enable \
+    iam.googleapis.com \
+    storage-api.googleapis.com \
+    cloudresourcemanager.googleapis.com \
+    compute.googleapis.com \
+    serviceusage.googleapis.com \
+    --project "${GCP_PROJECT_ID}"
+}
