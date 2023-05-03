32bd5bb94b8cfa6e7da3dd1e24e50b326899c0e9
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2023-05-03 17:24:14 +0200

    feat(colima): add colima startup script alias

--- roles/mac_dev_playbook/files/.zshrc.d/core/50_colima.zsh
+++ roles/mac_dev_playbook/files/.zshrc.d/core/50_colima.zsh
@@ -1,3 +1,10 @@
 #!/usr/bin/env zsh
 
 export DOCKER_HOST="unix:///Users/ANTOINE/.colima/default/docker.sock"
+
+function colima_up(){
+  colima status || colima start
+}
+
+# Start colima if not started
+alias docker="colima_up docker"
