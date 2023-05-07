a4ada9079fb8085a68b383490775c971fb98ea32
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2023-05-07 14:09:59 +0200

    feat(integ): add var for integ tests

--- roles/mac_dev_playbook/files/HOME/.zshrc.d/integ-tests/50_load_config.zsh
+++ roles/mac_dev_playbook/files/HOME/.zshrc.d/integ-tests/50_load_config.zsh
@@ -1,8 +1,11 @@
 #!/usr/bin/env zsh
 
 export GCP_PROJECT_ID=$(creds_get_local_secret "com.ameausoone.integ.tests" "gcp_project_id")
+export TF_VAR_gcp_project_id=$GCP_PROJECT_ID
 export GCP_REGION=$(creds_get_local_secret "com.ameausoone.integ.tests" "gcp_region")
 export SERVICE_ACCOUNT_JSON=$(creds_get_local_secret "com.ameausoone.integ.tests" "gcp_service_account")
+
+ gcloud config configurations activate integ-tests
 
 function googlecloud_enable_gcp_apis(){
   gcloud services enable \
