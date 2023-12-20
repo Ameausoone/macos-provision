dcf56e41f2342ed9432f2e81c36d45ccf18b5f76
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2023-12-20 11:16:58 +0100

    feat(git): add generated folder in .gitignore

--- roles/mac_dev_playbook/files/.zshrc.d/core/20_golang.zsh
+++ roles/mac_dev_playbook/files/.zshrc.d/core/20_golang.zsh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 
-export PATH="${HOME}/go/bin:${PATH}"
-export GOROOT=/usr/local/opt/go/libexec
-export GOPATH=~/go/
+#export PATH="${HOME}/go/bin:${PATH}"
+#export GOROOT=/usr/local/opt/go/libexec
+#export GOPATH=~/go/
 export GO111MODULE=on
 
 alias got="go test"
