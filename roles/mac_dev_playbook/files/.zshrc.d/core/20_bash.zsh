1393dca958daaa67edb633fc19b26fb4ee95d463
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2024-02-01 15:16:31 +0100

    feat(bash): ensure using latest bash version instead of 2007 macos version

---
+++ roles/mac_dev_playbook/files/.zshrc.d/core/20_bash.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+# Ensure that /usr/local/bin is prioritized over /usr/bin in the PATH
+# Notably, this ensure that /usr/bin/env bash uses the latest version of bash
+export PATH="/usr/local/bin:$PATH"
