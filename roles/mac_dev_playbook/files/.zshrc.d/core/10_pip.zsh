be4be415befcff6482a7cc77daa432715e130a34
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2024-02-01 15:50:10 +0100

    fix(python): back installation to brew

--- roles/mac_dev_playbook/files/.zshrc.d/core/10_pip.zsh
+++ roles/mac_dev_playbook/files/.zshrc.d/core/10_pip.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
-# add pip install path to PATH
-export PIP_DIR="$(asdf where python)/bin"
-export PATH="${PIP_DIR}:${PATH}"
+# add pip
+# I'm not using python through asdf due to a bug during python installation, I had to back to brew.
+# export PIP_DIR="$(asdf where python)/bin"
+# export PATH="${PIP_DIR}:${PATH}"
