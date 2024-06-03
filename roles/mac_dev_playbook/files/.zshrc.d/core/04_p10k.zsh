540f09310a4b540258729e638150bf3985ee76bf
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2024-06-03 10:47:35 +0200

    feat(p10k): reorganize

--- roles/mac_dev_playbook/files/.zshrc.d/core/04_p10k.zsh
+++ roles/mac_dev_playbook/files/.zshrc.d/core/04_p10k.zsh
@@ -6,6 +6,3 @@
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
---
+++ roles/mac_dev_playbook/files/.zshrc.d/core/85_p10k.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
