687679e568db528108a90b5b4297c6746630e82c
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2024-06-03 11:15:14 +0200

    fix(p10k): fix configuration

--- roles/mac_dev_playbook/files/.zshrc.d/core/85_p10k.zsh
+++ roles/mac_dev_playbook/files/.zshrc.d/core/85_p10k.zsh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
 
+source /usr/local/share/powerlevel10k/powerlevel10k.zsh-theme
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
