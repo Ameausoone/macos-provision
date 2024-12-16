ece54baed09cd51aff4a6a7680b4d870a6c42afa
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2024-12-16 11:20:13 +0100

    feat(zshrc): add GitHub email and SSH key generation alias
    Added GITHUB_EMAIL environment variable and an alias for generating SSH keys using the specified email. This streamlines SSH key creation for GitHub and improves configuration management.
    
    Signed-off-by: Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>

--- roles/mac_dev_playbook/files/.zshrc.d/core/50_git.zsh
+++ roles/mac_dev_playbook/files/.zshrc.d/core/50_git.zsh
@@ -5,6 +5,11 @@ alias gcml="gcm && gl"
 ### Editor ###
 export EDITOR='idea -e --wait'
 
+export GITHUB_EMAIL="592978+Ameausoone@users.noreply.github.com"
+
+# generate ssh-key
+alias gen-ssh-key="ssh-keygen -t ed25519 -C ${GITHUB_EMAIL}"
+
 function gcamp() {
   git commit -am "$1"
   git push
