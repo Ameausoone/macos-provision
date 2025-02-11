a2dc5aa52e9806aa2cde94b76cd538408c1e04b7
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2025-02-11 16:46:27 +0100

    feat(venv): add alias

--- roles/mac_dev_playbook/files/.zshrc.d/core/50_python.zsh
+++ roles/mac_dev_playbook/files/.zshrc.d/core/50_python.zsh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 
 alias python_setup_venv="python3 -m venv .venv --upgrade-deps && source .venv/bin/activate && pip install -r requirements.txt"
+alias setup_venv=python_setup_venv
