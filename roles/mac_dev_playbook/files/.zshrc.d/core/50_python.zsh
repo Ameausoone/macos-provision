86bc5337c7e9108c43222088d5cc7be6d8ad2575
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2025-02-25 15:04:44 +0100

    feat(git): update gitconfig stuff

--- roles/mac_dev_playbook/files/.zshrc.d/core/50_python.zsh
+++ roles/mac_dev_playbook/files/.zshrc.d/core/50_python.zsh
@@ -2,3 +2,5 @@
 
 alias python_setup_venv="python3 -m venv .venv --upgrade-deps && source .venv/bin/activate && pip install -r requirements.txt"
 alias setup_venv=python_setup_venv
+
+export PATH="$(asdf where python)/bin/:${PATH}"
