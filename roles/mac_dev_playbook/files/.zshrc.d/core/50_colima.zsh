87119e64f1376b12783d94c52402abbd97e06e81
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2025-09-02 16:27:29 +0200

    feat(colima): start automatically colima

--- roles/mac_dev_playbook/files/.zshrc.d/core/50_colima.zsh
+++ roles/mac_dev_playbook/files/.zshrc.d/core/50_colima.zsh
@@ -6,5 +6,15 @@ function colima_up(){
   colima status || colima start
 }
 
+# Asynchronous colima startup function
+function colima_start_async() {
+  # colima start is idempotent - won't start if already running
+  (colima start &>/dev/null) &
+  disown
+}
+
+# Auto-start colima asynchronously on shell init
+colima_start_async
+
 # Start colima if not started
 # alias docker="colima_up docker"
