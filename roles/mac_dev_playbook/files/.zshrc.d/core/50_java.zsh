e09549509520e185c35d18b5d0b839aecb0d24f6
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2025-11-12 11:40:41 +0100

    feat(java): add some logs for mise integration

--- roles/mac_dev_playbook/files/.zshrc.d/core/50_java.zsh
+++ roles/mac_dev_playbook/files/.zshrc.d/core/50_java.zsh
@@ -6,8 +6,10 @@ JDK_DIR="/Library/Java/JavaVirtualMachines/${JAVA_VERSION}.jdk"
 
 if [[ ! -d "$JDK_DIR" ]]; then
     sudo mkdir -p "$JDK_DIR"
+    echo "Created JDK directory at $JDK_DIR for mise/MacOs Java integration."
 fi
 
 if [[ ! -L "$JDK_DIR/Contents" ]]; then
     sudo ln -sf "$(mise where java)/Contents" "$JDK_DIR/Contents"
+    echo "Linked $(mise where java)/Contents to $JDK_DIR/Contents for mise/MacOs Java integration."
 fi
