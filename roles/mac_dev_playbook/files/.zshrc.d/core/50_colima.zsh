c4caea2be9972fbce96173ae06299282424e1564
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2025-11-18 10:44:39 +0100

    feat(rancher): replace colima by rancher

--- roles/mac_dev_playbook/files/.zshrc.d/core/50_colima.zsh
+++ roles/mac_dev_playbook/files/.zshrc.d/core/50_colima.zsh
@@ -1,17 +1,17 @@
 #!/usr/bin/env zsh
 
-export DOCKER_HOST="unix:///Users/${USERNAME}/.colima/default/docker.sock"
-
-function colima_up(){
-  colima status || colima start
-}
-
-# Asynchronous colima startup function
-function colima_start_async() {
-  # colima start is idempotent - won't start if already running
-  (colima start &>/dev/null) &
-  disown
-}
+# export DOCKER_HOST="unix:///Users/${USERNAME}/.colima/default/docker.sock"
+#
+# function colima_up(){
+#   colima status || colima start
+# }
+#
+# # Asynchronous colima startup function
+# function colima_start_async() {
+#   # colima start is idempotent - won't start if already running
+#   (colima start &>/dev/null) &
+#   disown
+# }
 
 # Auto-start colima asynchronously on shell init
 # colima_start_async
