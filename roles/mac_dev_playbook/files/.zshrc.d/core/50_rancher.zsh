54ce6a24c2eed2e3d4b184a14fb33cfd7565b94f
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2025-11-24 08:46:30 +0100

    feat(rancher): fix link

--- roles/mac_dev_playbook/files/.zshrc.d/core/50_rancher.zsh
+++ roles/mac_dev_playbook/files/.zshrc.d/core/50_rancher.zsh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
-if [[ ! -e /var/run/docker.sock ]]; then
+if [[ ! -L /var/run/docker.sock ]] || [[ "$(readlink /var/run/docker.sock)" != "$HOME/.rd/docker.sock" ]]; then
+  sudo rm -f /var/run/docker.sock
   sudo ln -s $HOME/.rd/docker.sock /var/run/docker.sock
   echo "Linked $HOME/.rd/docker.sock to /var/run/docker.sock for Rancher Desktop Docker integration."
 fi
