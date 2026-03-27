a07511e18f8d938d4f86f5f763d8232deee77aad
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2026-03-27 14:58:29 +0100

    chore(dotfiles): inline fnox shell integration instead of eval activate

--- roles/mac_dev_playbook/files/HOME/.zshrc.d/core/50_fnox.zsh
+++ roles/mac_dev_playbook/files/HOME/.zshrc.d/core/50_fnox.zsh
@@ -3,4 +3,41 @@
 # Enable fnox shell integration
 # fnox is a tool to load secrets automatically, integrated with mise
 # https://fnox.jdx.dev/
-eval "$(fnox activate zsh)"
+#eval "$(fnox activate zsh)"
+
+export FNOX_SHELL=zsh
+
+fnox() {
+  local command
+  command="${1:-}"
+  if [ "$#" = 0 ]; then
+    ~/.local/share/mise/installs/fnox/latest/fnox
+    return
+  fi
+  shift
+
+  case "$command" in
+  deactivate|shell)
+    eval "$(~/.local/share/mise/installs/fnox/latest/fnox "$command" "$@")"
+    ;;
+  *)
+    ~/.local/share/mise/installs/fnox/latest/fnox "$command" "$@"
+    ;;
+  esac
+}
+
+_fnox_hook() {
+  trap -- '' SIGINT
+  eval "$(~/.local/share/mise/installs/fnox/latest/fnox hook-env -s zsh)"
+  trap - SIGINT
+}
+
+typeset -ag precmd_functions
+if [[ -z "${precmd_functions[(r)_fnox_hook]+1}" ]]; then
+  precmd_functions=( _fnox_hook ${precmd_functions[@]} )
+fi
+
+typeset -ag chpwd_functions
+if [[ -z "${chpwd_functions[(r)_fnox_hook]+1}" ]]; then
+  chpwd_functions=( _fnox_hook ${chpwd_functions[@]} )
+fi
